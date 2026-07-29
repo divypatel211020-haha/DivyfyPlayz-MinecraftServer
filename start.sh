eb9954cb1a4ba70cd53af887fa1967c02c90e6ac
Divy Patel <309779548+divypatel211020-haha@users.noreply.github.com>
2026-07-29 17:02:34 +0530

    Simplify server startup script and adjust memory
    Removed the loop for server rebooting and updated memory allocation.

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Automatically checks and boots up the Paper server with max memory allocation
-echo "Starting Minecraft Crossplay Server..."
-while true
-do
-    java -Xms2G -Xmx3G -XX:+UseG1GC -jar paper.jar nogui
-    echo "Server rebooting in 5 seconds... Press CTRL+C to stop."
-    sleep 5
-done
+java -Xms1G -Xmx2G -XX:+UseG1GC -jar server.jar nogui
